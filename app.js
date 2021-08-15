b7f2b3da2b7107e185e9f45690372316b126198d
Robert M <glasgow525@gmail.com>
2021-08-15 09:36:47 -0430

    cleaning

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-
 function conversion(precio){
 	let self = this; 
 	self.input = ko.observable(0);
-	self.numero = ko.computed(function(){
-		let conversor = self.input() * precio;
-			return conversor;
+	self.numero = ko.computed(() => {
+
+		if(self.input() >= 0){
+			let conversor = self.input() * precio;
+			return conversor.toFixed(2);
+		}else return conversor = `error`;
+
 	},self);
 }
 
